08a6a306cae74dbb4cd4be09467c7e8a423319e4
Massimiliano Caniparoli <massic80@users.noreply.github.com>
2017-11-06 18:03:55 +0100

    Ignoring disabled checkboxes
    Disabled checkboxes shouldn't be considered in master changes:
    - when user clicks the master button, only the enabled checkboxes are checked/unchecked
    - when the areAllGroupItems(Un)Checked functions are run, only the enabled checkboxes are parsed.

--- js/jquery.ssdThreeStateCheckbox.js
+++ js/jquery.ssdThreeStateCheckbox.js
@@ -7,302 +7,227 @@
  * Requires: jQuery v1.9 or later
  */
 (function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module unless amdModuleId is set
-        define(['jquery'], function (a0) {
-            return (factory(a0));
-        });
-    } else if (typeof exports === 'object') {
-        // Node. Does not work with strict CommonJS, but
-        // only CommonJS-like environments that support module.exports,
-        // like Node.
-        module.exports = factory(require('jquery'));
-    } else {
-        factory(root["jQuery"]);
-    }
+	if (typeof define === 'function' && define.amd) {
+		// AMD. Register as an anonymous module unless amdModuleId is set
+		define(['jquery'], function (a0) {
+			return (factory(a0));
+		});
+	} else if (typeof exports === 'object') {
+		// Node. Does not work with strict CommonJS, but
+		// only CommonJS-like environments that support module.exports,
+		// like Node.
+		module.exports = factory(require('jquery'));
+	} else {
+		factory(root["jQuery"]);
+	}
 }(this, function ($) {
 
-    (function() {
+	(function() {
 
-        $.fn.ssdThreeStateCheckbox = function(options) {
+		$.fn.ssdThreeStateCheckbox = function(options) {
 
-            "use strict";
+			"use strict";
 
-            var settings = $.extend({
+			var settings = $.extend({
 
-                classButton         : 'checkboxButton',
-                classMaster         : 'checkboxMaster',
-                classRecord         : 'checkboxRecord',
+				classButton			: 'checkboxButton',
+				classMaster			: 'checkboxMaster',
+				classRecord			: 'checkboxRecord',
 
-                dataGroupButton     : 'group-button',
-                dataGroupMaster     : 'group-master',
-                dataGroupRecord     : 'group-record',
-                dataGroupRecordId   : 'id',
+				dataGroupButton		: 'group-button',
+				dataGroupMaster		: 'group-master',
+				dataGroupRecord		: 'group-record',
+				dataGroupRecordId	: 'id',
 
-                clickCallback       : function(ids, trigger) {
+				clickCallback		: function(ids, trigger) {
 
-                    console.log("clickCallback method call : collected [" + ids + "]");
+					console.log("clickCallback method call : collected [" + ids + "]");
 
-                }
+				}
 
-            }, options);
+			}, options);
 
 
-            function isEmpty(thisValue) {
+			function isEmpty(thisValue) {
 
-                return (
-                    thisValue === '' ||
-                    typeof thisValue === 'undefined'
-                );
+				return (
+					thisValue === '' ||
+					typeof thisValue === 'undefined'
+				);
 
-            }
+			}
 
-            function enableButton(thisGroup) {
+			function enableButton(thisGroup) {
 
-                "use strict";
+				"use strict";
 
-                var identity = '[data-'
-                    + settings.dataGroupButton
-                    + '="'
-                    + thisGroup
-                    + '"].'
-                    + settings.classButton;
+				var identity = '[data-'
+					+ settings.dataGroupButton
+					+ '="'
+					+ thisGroup
+					+ '"].'
+					+ settings.classButton;
 
-                $(identity).each(function() {
-                    
-                    var $this = $(this);
-                    
-                    $this.removeClass('disabled');
+				$(identity).each(function() {
+					var $this = $(this);
+					$this.removeClass('disabled');
+					if ($this.is(':input'))
+						$this.prop('disabled', false);
+				});
+			}
 
-                    if ($this.is(':input')) {
+			function disableButton(thisGroup) {
 
-                        $this.prop('disabled', false);
+				"use strict";
 
-                    }
+				var identity = '[data-'
+					+ settings.dataGroupButton
+					+ '="'
+					+ thisGroup
+					+ '"].'
+					+ settings.classButton;
 
-                });
+				$(identity).each(function() {
 
-            }
+					var $this = $(this);
 
-            function disableButton(thisGroup) {
+					$this.addClass('disabled');
 
-                "use strict";
+					if ($this.is(':input'))
+						$this.prop('disabled', true);
+				});
+			}
 
-                var identity = '[data-'
-                    + settings.dataGroupButton
-                    + '="'
-                    + thisGroup
-                    + '"].'
-                    + settings.classButton;
+			function areAllGroupItemsChecked(thisGroupItems) {
+				"use strict";
+				var enabled = thisGroupItems.not('[disabled]');
+				return (enabled.length === enabled.filter(':checked').length);
+			}
+
+			function areAllGroupItemsUnChecked(thisGroupItems) {
+				"use strict";
+				var enabled = thisGroupItems.not('[disabled]');
+				return (enabled.length === enabled.not(':checked').length);
+			}
+
+    		function setMasterObject(thisGroup) {
+    			"use strict";
+				return $('input[data-' + settings.dataGroupMaster + '="' + thisGroup + '"].' + settings.classMaster);
+			}
 
-                $(identity).each(function() {
-                    
-                    var $this = $(this);
-                    
-                    $this.addClass('disabled');
-
-                    if ($this.is(':input')) {
-
-                        $this.prop('disabled', true);
-
-                    }
-
-                });
-
-            }
-
-            function areAllGroupItemsChecked(thisGroupItems) {
-
-                "use strict";
-
-                var checked = thisGroupItems.filter(':checked');
-
-                return (checked.length === thisGroupItems.length);
-
-            }
-
-            function areAllGroupItemsUnChecked(thisGroupItems) {
-
-                "use strict";
-
-                var unChecked = thisGroupItems.filter(':not(:checked)');
-
-                return (unChecked.length === thisGroupItems.length);
-
-            }
-
-            function setMasterObject(thisGroup) {
-
-                "use strict";
-
-                return $('input[data-' + settings.dataGroupMaster + '="' + thisGroup + '"].' + settings.classMaster);
-
-            }
-
-            function setRecordObject(thisGroup) {
-
-                "use strict";
-
-                return $('input[data-' + settings.dataGroupRecord + '="' + thisGroup + '"].' + settings.classRecord);
-
-            }
-
-            function setCheckedRecordObject(thisGroup) {
-
-                "use strict";
-
-                return $('input:checked[data-' + settings.dataGroupRecord + '="' + thisGroup + '"].' + settings.classRecord);
-
-            }
-
-            function getSelectedIds(thisGroup) {
-
-                "use strict";
-
-                var deferred = $.Deferred(),
-                    groupItems = setCheckedRecordObject(thisGroup),
-                    ids = [];
-
-                groupItems.each(function(k, v) {
-
-                    ids.push($(this).data(settings.dataGroupRecordId));
-
-                    if (parseInt((k + 1), 10) === groupItems.length) {
-
-                        deferred.resolve(ids);
-
-                    }
-
-                });
-
-                return deferred.promise();
-
-            }
-
-            function registerEvents() {
-
-                "use strict";
-
-                $(document).on('change', '.' + settings.classMaster, function() {
-
-                    var $this = $(this),
-                        group = $this.data(settings.dataGroupMaster),
-                        groupItems = setRecordObject(group);
-
-                    if ($this.is(':checked')) {
-
-                        groupItems.prop('checked', true);
-
-                        enableButton(group);
-
-                    } else {
-
-                        groupItems.prop('checked', false);
-
-                        disableButton(group);
-
-                    }
-
-                });
-
-                $(document).on('change', '.' + settings.classRecord, function() {
-
-                    var group = $(this).data(settings.dataGroupRecord),
-                        groupItems = setRecordObject(group),
-                        groupMaster = setMasterObject(group);
-
-                    setMasterStatus(group, groupMaster, groupItems);
-
-                });
-
-                $(document).on('click', '.' + settings.classButton, function(event) {
-
-                    event.preventDefault();
-
-                    if (!$(this).hasClass('disabled')) {
-
-                        var trigger = $(this),
-                            group = trigger.data(settings.dataGroupButton);
-
-
-                        $.when(getSelectedIds(group)).done(function(ids) {
-
-                            settings.clickCallback(ids, trigger);
-
-                        });
-
-                    }
-
-                });
-
-            }
-
-            function setMasterStatus(group, groupMaster, groupItems) {
-
-                "use strict";
-
-                if (areAllGroupItemsUnChecked(groupItems)) {
-
-                    groupMaster.prop({
-                        indeterminate: false,
-                        checked: false
-                    });
-
-                    disableButton(group);
-
-                } else if (areAllGroupItemsChecked(groupItems)) {
-
-                    groupMaster.prop({
-                        indeterminate: false,
-                        checked: true
-                    });
-
-                    enableButton(group);
-
-                } else {
-
-                    groupMaster.prop({
-                        indeterminate: true,
-                        checked: false
-                    });
-
-                    enableButton(group);
-
-                }
-            }
-
-            function initialise() {
-
-                "use strict";
-
-                $('.' + settings.classMaster).each(function() {
-
-                    var $this = $(this),
-                        group = $this.data(settings.dataGroupMaster),
-                        groupItems = setRecordObject(group);
-
-                    if (groupItems.length === 0) {
-                        return;
-                    }
-
-                    setMasterStatus(group, $this, groupItems);
-
-                });
-
-            }
-
-            function boot() {
-
-                "use strict";
-
-                initialise();
-                registerEvents();
-
-            }
-
-            return boot();
-        }
-
-    })();
-
+			function setRecordObject(thisGroup) {
+				"use strict";
+				return $('input.' + settings.classRecord + '[data-' + settings.dataGroupRecord + '="' + thisGroup + '"]');
+			}
+
+			function setCheckedRecordObject(thisGroup) {
+				"use strict";
+				return setRecordObject(thisGroup).filter(':checked');
+			}
+
+			function getSelectedIds(thisGroup) {
+				"use strict";
+				var deferred = $.Deferred(),
+					groupItems = setCheckedRecordObject(thisGroup),
+					ids = [];
+
+				groupItems.each(function(k, v) {
+					ids.push($(this).data(settings.dataGroupRecordId));
+
+					if (parseInt((k + 1), 10) === groupItems.length)
+						deferred.resolve(ids);
+				});
+
+				return deferred.promise();
+			}
+
+			function registerEvents() {
+
+				"use strict";
+
+				$(document).on('change', '.' + settings.classMaster, function() {
+
+					var $this = $(this),
+						group = $this.data(settings.dataGroupMaster),
+						groupItems = setRecordObject(group);
+
+					if ($this.is(':checked')) {
+						groupItems.not('[disabled]').prop('checked', true);
+						enableButton(group);
+					} else {
+    					groupItems.not('[disabled]').prop('checked', false);
+    					disableButton(group);
+					}
+
+				});
+
+				$(document).on('change', '.' + settings.classRecord, function() {
+					var group = $(this).data(settings.dataGroupRecord),
+						groupItems = setRecordObject(group),
+						groupMaster = setMasterObject(group);
+
+					setMasterStatus(group, groupMaster, groupItems);
+				});
+
+				$(document).on('click', '.' + settings.classButton, function(event) {
+					event.preventDefault();
+					if (!$(this).hasClass('disabled')) {
+						var trigger = $(this),
+							group = trigger.data(settings.dataGroupButton);
+
+						$.when(getSelectedIds(group)).done(function(ids) {
+							settings.clickCallback(ids, trigger);
+						});
+					}
+				});
+			}
+
+			function setMasterStatus(group, groupMaster, groupItems) {
+				"use strict";
+				if (areAllGroupItemsUnChecked(groupItems)) {
+					groupMaster.prop({
+						indeterminate: false,
+						checked: false
+					});
+					disableButton(group);
+				} else if (areAllGroupItemsChecked(groupItems)) {
+
+					groupMaster.prop({
+						indeterminate: false,
+						checked: true
+					});
+					enableButton(group);
+				} else {
+					groupMaster.prop({
+						indeterminate: true,
+						checked: false
+					});
+					enableButton(group);
+				}
+			}
+
+			function initialise() {
+				"use strict";
+				$('.' + settings.classMaster).each(function() {
+
+					var $this = $(this),
+						group = $this.data(settings.dataGroupMaster),
+						groupItems = setRecordObject(group);
+
+					if (groupItems.length === 0)
+						return;
+
+					setMasterStatus(group, $this, groupItems);
+				});
+
+			}
+
+			function boot() {
+				"use strict";
+				initialise();
+				registerEvents();
+			}
+			return boot();
+		}
+	})();
 }));
